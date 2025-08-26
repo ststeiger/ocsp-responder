2d7388dfe189ddf0d499533a3debae49f67bae43
Stella Artois <officedev@gmx.ch>
2025-08-26 15:22:02 +0200

    Fix: Remove deleted project

--- xxxOcspResponder.Core/IOcspResponder.cs
+++
@@ -1,9 +0,0 @@
-
-
-namespace OcspResponder.Core
-{
-    public interface IOcspResponder
-    {
-        System.Threading.Tasks.Task<OcspHttpResponse> Respond(OcspHttpRequest httpRequest);
-    }
-}
